8e82d93fa2bd5e661817c0f67a29bee9f471d1e8
theMagnificentJay <reedjoshua408@gmail.com>
2020-09-10 14:16:35 -0500

    first push

---
+++ grammarTypes/02-declarations.js
@@ -0,0 +1,41 @@
+/*
+*********************************
+! DECLARATIONS & INITIALIZATIONS
+*********************************
+*/
+// Declarations are the LEFT SIDE of the assignment operator
+// let x;
+
+// Initializations are the RIGHT SIDE of the assignment operator
+// let x = 10;
+
+// 10 is our initialization
+
+let y;
+console.log('declaration:', y);
+
+y = 10;
+console.log('initialization: ', y);
+
+y = 33;
+console.log('initialization 2: ', y);
+
+// We've set our variables with our let keyword. With each iteration, we have declared what it is.
+
+//? const
+
+let today = 'Great!';
+const efa = 'Wonderful!';
+console.log(today, efa);
+
+today = 'Awesome!';
+console.log(today, efa);
+
+// Declare and initialize in the same line below
+// let z = 1;
+// 'let z' is the declaration, '= 1;' is the initialization
+
+// efa = 'Super';
+console.log(today, efa);
+// const allows us to make a variable that we don't want to change.
+
